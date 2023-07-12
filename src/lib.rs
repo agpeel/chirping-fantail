f343e6a89e10fcd8220eff3b78f8dcf73174aad1
Andrew Peel <andrew@andrewpeel.id.au>
2023-07-12 22:47:32 +1000

    Added a constructor for PokerHandHandle.
    Will handle invalid hand strings and classify the hand type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod poker_hand;
 
+use poker_hand::build_poker_hand_handle;
 use poker_hand::PokerHandHandle;
 
 /// Given a list of poker hands, return a list of those hands which win.
@@ -10,7 +11,12 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
     // Record the hand handles in a mutable vector that will be sorted.
     let mut hand_handles: Vec<PokerHandHandle> = Vec::with_capacity(hands.len());
     for hand in hands {
-        hand_handles.push(PokerHandHandle { hand_handle: hand });
+        let hand_handle = match build_poker_hand_handle(hand) {
+            Ok(hand_handle) => hand_handle,
+            // Ignore invalid hands.
+            Err(_) => continue,
+        };
+        hand_handles.push(hand_handle);
     }
 
     hand_handles.sort_by(|a, b| a.partial_cmp(b).unwrap());
--- src/poker_hand.rs
+++ src/poker_hand.rs
@@ -1,9 +1,40 @@
 use std::cmp::Ordering;
 
+#[derive(Debug)]
+pub enum PokerHandRank {
+    HighCard,
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+}
+
 // Record a reference to the input string slice and derived information about the hand.
 #[derive(Debug)]
 pub struct PokerHandHandle<'a> {
     pub hand_handle: &'a str,
+    pub hand_rank: PokerHandRank,
+    // A vector of the card ranks (the number, not the suit) in the hand, sorted in "scoring order".
+    // Scoring order means the order in which the cards are compared to determine the winner.
+    // For example "9H 2S 3C 3D 5H" would be [3, 3, 9, 5, 2]
+    pub card_ranks: Vec<u8>,
+}
+
+pub fn build_poker_hand_handle(hand: &str) -> Result<PokerHandHandle, &'static str> {
+    let mut card_ranks: Vec<u8> = Vec::with_capacity(5);
+    let mut hand_rank: PokerHandRank = PokerHandRank::HighCard;
+
+    // TODO: Parse the hand string.  Return None for invalid hand strings.
+
+    Ok(PokerHandHandle {
+        hand_handle: hand,
+        hand_rank,
+        card_ranks,
+    })
 }
 
 impl PartialEq for PokerHandHandle<'_> {
