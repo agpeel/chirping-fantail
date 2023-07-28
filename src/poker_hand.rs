75201a83ec5f029e0fcdcdd345aca7584d250700
Andrew Peel <andrew@andrewpeel.id.au>
2023-07-28 16:16:00 +1000

    Parse the hand string.
    Parse the string with the regex.
    Convert the string items to suit and rank types for each card.

--- src/poker_hand.rs
+++ src/poker_hand.rs
@@ -1,5 +1,7 @@
 use crate::cards::{Card, Ranks, Suits};
 use crate::error::PokerHandError;
+use core::num::dec2flt::parse;
+use regex::Regex;
 use std::cmp::Ordering;
 
 /// Poker hand types, in the order of their relative value.
@@ -35,10 +37,16 @@ pub struct PokerHand<'a> {
 
 impl PokerHand<'_> {
     pub fn new(hand: &str) -> Result<PokerHand, PokerHandError> {
-        let mut cards: Vec<Card> = Vec::with_capacity(5);
+        let mut cards: Vec<Card>;
 
         // Parse the hand string.
-        todo!();
+        match PokerHand::parse_hand_str(hand) {
+            Some(parsed_cards) => {
+                cards = parsed_cards;
+            }
+            None => return Err(PokerHandError::new("Invalid poker hand")),
+        }
+        // TODO: Check for the same card in the hard.
 
         // Classify the hand.
         let hand_rank: PokerHandRanks = PokerHandRanks::HighCard;
@@ -49,6 +57,64 @@ impl PokerHand<'_> {
             hand_rank,
             cards,
         })
+    }
+
+    fn convert_strings_to_card(rank: &str, suit: &str) -> Card {
+        let card_rank: Ranks;
+        let card_suit: Suits;
+        match rank {
+            "2" => card_rank = Ranks::Two,
+            "3" => card_rank = Ranks::Three,
+            "4" => card_rank = Ranks::Four,
+            "5" => card_rank = Ranks::Five,
+            "6" => card_rank = Ranks::Six,
+            "7" => card_rank = Ranks::Seven,
+            "8" => card_rank = Ranks::Eight,
+            "9" => card_rank = Ranks::Nine,
+            "10" => card_rank = Ranks::Ten,
+            "J" => card_rank = Ranks::Jack,
+            "Q" => card_rank = Ranks::Queen,
+            "K" => card_rank = Ranks::King,
+            "A" => card_rank = Ranks::Ace,
+            _ => panic!("Invalid card rank"),
+        }
+        match suit {
+            "H" => card_suit = Suits::Hearts,
+            "S" => card_suit = Suits::Spades,
+            "C" => card_suit = Suits::Clubs,
+            "D" => card_suit = Suits::Diamonds,
+            _ => panic!("Invalid card suit"),
+        }
+        Card::new(card_rank, card_suit)
+    }
+
+    fn parse_hand_str(hand: &str) -> Option<Vec<Card>> {
+        let mut cards: Vec<Card> = Vec::with_capacity(5);
+
+        let re = Regex::new(r"^(?<rank1>[2-9]|10|[JQKA])(?<suit1>[HSCD]) (?<rank2>[2-9]|10|[JQKA])(?<suit2>[HSCD]) (?<rank3>[2-9]|10|[JQKA])(?<suit3>[HSCD]) (?<rank4>[2-9]|10|[JQKA])(?<suit4>[HSCD]) (?<rank5>[2-9]|10|[JQKA])(?<suit5>[HSCD])$").unwrap();
+        let Some(caps) = re.captures(hand) else { return None; };
+        cards.push(PokerHand::convert_strings_to_card(
+            &caps["rank1"],
+            &caps["suit1"],
+        ));
+        cards.push(PokerHand::convert_strings_to_card(
+            &caps["rank2"],
+            &caps["suit2"],
+        ));
+        cards.push(PokerHand::convert_strings_to_card(
+            &caps["rank3"],
+            &caps["suit3"],
+        ));
+        cards.push(PokerHand::convert_strings_to_card(
+            &caps["rank4"],
+            &caps["suit4"],
+        ));
+        cards.push(PokerHand::convert_strings_to_card(
+            &caps["rank5"],
+            &caps["suit5"],
+        ));
+
+        Some(cards)
     }
 }
 
@@ -136,5 +202,48 @@ mod tests {
             ],
         };
         assert!(hand3 == hand1);
+    }
+
+    #[test]
+    fn test_parse_hand_str() {
+        let hand_str = "9H AS JC 10D 5H";
+        let cards = PokerHand::parse_hand_str(hand_str).unwrap();
+        assert_eq!(cards[0].rank, Ranks::Nine);
+        assert_eq!(cards[0].suit, Suits::Hearts);
+        assert_eq!(cards[1].rank, Ranks::Ace);
+        assert_eq!(cards[1].suit, Suits::Spades);
+        assert_eq!(cards[2].rank, Ranks::Jack);
+        assert_eq!(cards[2].suit, Suits::Clubs);
+        assert_eq!(cards[3].rank, Ranks::Ten);
+        assert_eq!(cards[3].suit, Suits::Diamonds);
+        assert_eq!(cards[4].rank, Ranks::Five);
+        assert_eq!(cards[4].suit, Suits::Hearts);
+    }
+    #[test]
+    fn test_parse_hand_str_invalid_rank() {
+        let hand_str = "9H AS JC 12D 5H";
+        let result = PokerHand::parse_hand_str(hand_str);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_parse_hand_str_invalid_suit() {
+        let hand_str = "9H AS JK 10D 5H";
+        let result = PokerHand::parse_hand_str(hand_str);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_parse_hand_str_not_enough_cards() {
+        let hand_str = "9H AS JC 10D";
+        let result = PokerHand::parse_hand_str(hand_str);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_parse_hand_str_too_many_cards() {
+        let hand_str = "9H AS JC 10D 5H QS";
+        let result = PokerHand::parse_hand_str(hand_str);
+        assert_eq!(result, None);
     }
 }
